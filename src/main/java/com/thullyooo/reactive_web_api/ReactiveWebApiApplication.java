5242708adee7c2e2b975ea02199ed6dd4e3cee08
Thullyoo <thullyofilho@gmail.com>
2024-10-16 16:09:56 -0300

    Initial commit

---
+++ src/main/java/com/thullyooo/reactive_web_api/ReactiveWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.thullyooo.reactive_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebApiApplication.class, args);
+	}
+
+}
